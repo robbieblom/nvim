20ea84eefaf19ee45a12087f5bc0e8faff4f8e00
Robbie Blom <robbie.blom@bytetheoryinnovations.com>
2024-02-29 13:32:57 -0500

    added a remap for colon and replaced python lsp.

--- lua/config/core/remap.lua
+++ lua/config/core/remap.lua
@@ -36,6 +36,8 @@ vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 
+vim.keymap.set("n", "<leader>c", ":")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
--- lua/config/plugins/lsp/lsp-zero.lua
+++ lua/config/plugins/lsp/lsp-zero.lua
@@ -50,7 +50,7 @@ return {
           "jsonls",
           "tsserver",
           "marksman",
-          "pyre",
+          "pyright",
           "sqlls",
         },
         handlers = {
